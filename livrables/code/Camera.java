62092c1c878afc8a55f32d167bc50d344ebe098c
palkeo <contact@palkeo.com>
2013-06-05 16:09:13 +0000

    Implémentation de la classe Camera.

--- livrables/code/Camera.java
+++ livrables/code/Camera.java
@@ -38,11 +38,11 @@ public class Camera {
     }
 
     /**
-     * Get the value of heigthPixels
-     * @return the value of heigthPixels
+     * Get the value of heightPixels
+     * @return the value of heightPixels
      */
-    public int getHeigthPixels ( ) {
-        return heigthPixels;
+    public int getHeightPixels ( ) {
+        return heightPixels;
     }
 
     /**
@@ -50,8 +50,27 @@ public class Camera {
      */
     public Ray getRay(int x, int y)
     {
-        //TODO
-        Vector3d direction = new Vector3d();
-        return Ray(eye, direction);
+        Vector3d v = new Vector3d();
+        v.sub(screenPoint(x, y), eye);
+
+        return Ray(eye, v);
+    }
+
+    /**
+     * Renvoie les coordonnées spatiales d'un point sur l'écran
+     */
+    private Point3d screenPoint(int x, int y)
+    {
+        Point3d scaledAbscissa = new Point3d();
+        scaledAbscissa.scale(x / (double)widthPixels, abscissa);
+
+        Point3d scaledOrdinate = new Point3d();
+        scaledOrdinate.scale(y / (double)heightPixels, ordinate);
+
+        Point3d p = new Point3d(origin);
+        p.add(p, scaledAbscissa);
+        p.add(p, scaledOrdinate);
+
+        return p;
     }
 }
