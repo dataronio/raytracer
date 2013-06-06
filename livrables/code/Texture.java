f587bf92c9bf884fd8689e7c9db130e772154893
palkeo <contact@palkeo.com>
2013-06-06 15:18:55 +0000

    Ajout de la classe texture.

---
+++ livrables/code/Texture.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+public class Texture {
+
+    public double transparency;
+    public double absorbance;
+    public double reflectance;
+    public double refractiveIndex;
+    public java.awt.Color color;
+
+}
