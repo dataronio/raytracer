885f853aeab9de81019eeec5fb69e77832426f89
Maxime Arthaud <maxime@arthaud.me>
2013-06-11 16:46:32 +0000

    Fix: inversion de l'image dans bullet_time.py

--- scripting/utils/bullet_time.py
+++ scripting/utils/bullet_time.py
@@ -9,10 +9,10 @@ répertoire_destination = input("Répertoire des images : ")
 height = int(input("Taille : "))
 width = height * RATIO
 
-centre_x = int(input("Centre x : "))
-centre_y = int(input("Centre y : "))
-centre_z = int(input("Centre z : "))
-rayon = int(input("Rayon : "))
+centre_x = float(input("Centre x : "))
+centre_y = float(input("Centre y : "))
+centre_z = float(input("Centre z : "))
+rayon = float(input("Rayon : "))
 nb_images = int(input("Nombre d'images : "))
 
 scène_sans_caméra = [i for i in scène if not i.lower().startswith("camera")]
@@ -29,14 +29,14 @@ while angle < 2*math.pi:
     angle += step
     i += 1
 
-    pos_milieu_écran = (centre_x - rayon*math.cos(angle), centre_y - rayon*math.sin(angle), centre_z)
+    pos_milieu_écran = (centre_x + rayon*math.cos(angle), centre_y + rayon*math.sin(angle), centre_z)
 
     vecteur_abscisse = (math.cos(angle + math.pi/2), math.sin(angle + math.pi/2), 0)
-    vecteur_ordonnée = (0, 0, -1)
+    vecteur_ordonnée = (0, 0, 1)
 
     pos_debut_écran = (pos_milieu_écran[0] - vecteur_abscisse[0]/2, pos_milieu_écran[1] - vecteur_abscisse[1]/2, pos_milieu_écran[2] - vecteur_ordonnée[2]/2)
 
-    pos_œil = (centre_x - (rayon + 1)*math.cos(angle), centre_y - (rayon + 1)*math.sin(angle), centre_z)
+    pos_œil = (centre_x + (rayon + 1)*math.cos(angle), centre_y + (rayon + 1)*math.sin(angle), centre_z)
 
     ligne_caméra = "Camera(eye=%s, origin=%s, abscissa=%s, ordinate=%s, widthPixel=%s, heightPixel=%s)" % (repr(pos_œil), repr(pos_debut_écran), repr(vecteur_abscisse), repr(vecteur_ordonnée), width, height)
 
