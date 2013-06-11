7476006e0ea048b686d75840639305d6e43dbf95
palkeo <contact@palkeo.com>
2013-06-11 10:54:19 +0000

    Recodage du cube avec des rectangles et plus des triangles.

--- livrables/raytracer/Cube.java
+++ livrables/raytracer/Cube.java
@@ -25,14 +25,14 @@ public class Cube extends Mesh {
         Point3d p1_, Point3d p2_, Point3d p3_, Point3d p4_
     ) 
     {
-        /*     +-------p5
+        /*     p2------p5
          *    /|      /|
          *   / |     / |
-         *  +-------+  |
+         *  p1------p3 |
          *  |  p8---|--p7
          *  | /     | /
          *  |/      |/
-         *  +------p6
+         *  p4-----p6
          */
 
         // les points sont copiés afin d'éviter que p5..p8 ne deviennent
@@ -52,37 +52,28 @@ public class Cube extends Mesh {
 
         Vector3d p1p4 = new Vector3d(p1, p4);
 
-        Point3d p7 = new Point3d(p5);
-        p7.add(p1p4);
-
         Point3d p8 = new Point3d(p2);
         p8.add(p1p4);
 
         Set<BasicObject> triangles = new HashSet<BasicObject>(12);
 
         // up
-        triangles.add(new Triangle(texture, p1, p3, p2));
-        triangles.add(new Triangle(texture, p5, p2, p3));
+        triangles.add(new Parallelogram(texture, p1, p3, p2));
 
         // down
-        triangles.add(new Triangle(texture, p4, p8, p6));
-        triangles.add(new Triangle(texture, p7, p6, p8));
+        triangles.add(new Parallelogram(texture, p4, p8, p6));
 
         // front
-        triangles.add(new Triangle(texture, p1, p4, p6));
-        triangles.add(new Triangle(texture, p3, p1, p6));
+        triangles.add(new Parallelogram(texture, p1, p4, p3));
 
         // bottom
-        triangles.add(new Triangle(texture, p2, p7, p8));
-        triangles.add(new Triangle(texture, p2, p5, p7));
+        triangles.add(new Parallelogram(texture, p2, p8, p5));
 
         // left
-        triangles.add(new Triangle(texture, p1, p2, p8));
-        triangles.add(new Triangle(texture, p1, p8, p4));
+        triangles.add(new Parallelogram(texture, p1, p2, p4));
 
         // right
-        triangles.add(new Triangle(texture, p3, p5, p7));
-        triangles.add(new Triangle(texture, p3, p7, p6));
+        triangles.add(new Parallelogram(texture, p3, p5, p6));
 
         setObjects(triangles);
     }
